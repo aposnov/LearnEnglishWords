7337030475090e801a297abd1efe420725b1d76b
Andrey Posnov <mailbox@aposnov.ru>
2019-06-13 03:25:30 +0300

    Final now

--- LearnEnglishWords/History/HistoryInteractor.swift
+++ LearnEnglishWords/History/HistoryInteractor.swift
@@ -16,62 +16,30 @@ protocol HistoryBusinessLogic {
 class HistoryInteractor: HistoryBusinessLogic {
 
   var presenter: HistoryPresentationLogic?
-  var service: HistoryService?
+  var service: HistoryServiceCoreData?
   
   func makeRequest(request: History.Model.Request.RequestType) {
   
+    if service == nil {
+        service = HistoryServiceCoreData()
+    }
+    
         switch request {
           case .getVocabulary:
-            var vocabulary = [Words]()
-            vocabulary = getVocabulary()
+            if let vocabulary = service?.getVocabulary() {
             self.presenter?.presentData(response: .sendVocabulary(vocabulary: vocabulary))
+            }
           case .cleanVocabulary:
-            var vocabulary = [Words]()
-            vocabulary = cleanVocabulary()
+            if let vocabulary = service?.cleanVocabulary() {
             self.presenter?.presentData(response: .sendVocabulary(vocabulary: vocabulary))
+            }
           case .getVocabularyFiltered(let search):
-            var vocabulary = [Words]()
-            vocabulary = myFetchRequest(search: search)
+            if let vocabulary = service?.myFetchRequest(search: search) {
             self.presenter?.presentData(response: .sendVocabulary(vocabulary: vocabulary))
+            }
         }
  
   }
-    func myFetchRequest(search: String) -> [Words] {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        let fetchRequest: NSFetchRequest<Words> = Words.fetchRequest()
-        fetchRequest.predicate = NSPredicate (format: "sourceWord CONTAINS[cd] %@ OR targetWord CONTAINS[cd] %@", search, search)
-        return try! context.fetch(fetchRequest)
-    }
-    
-    func getVocabulary() -> [Words] {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        let fetchRequest: NSFetchRequest<Words> = Words.fetchRequest()
-        return try! context.fetch(fetchRequest)
-    }
-    
-    func cleanVocabulary() -> [Words] {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Words")
-        fetchRequest.returnsObjectsAsFaults = false
-        
-        do
-        {
-            let results = try context.fetch(fetchRequest)
-            for managedObject in results
-            {
-                let managedObjectData:NSManagedObject = managedObject as! NSManagedObject
-                context.delete(managedObjectData)
-            }
-        } catch let error as NSError {
-          //  print("Detele all my data in \(entity) error : \(error) \(error.userInfo)")
-        }
-        
-        let fetchRequestZ: NSFetchRequest<Words> = Words.fetchRequest()
-        return try! context.fetch(fetchRequestZ)
-    }
+   
   
 }
--- LearnEnglishWords/History/HistoryWorker.swift
+++ LearnEnglishWords/History/HistoryWorker.swift
@@ -7,7 +7,46 @@
 //
 
 import UIKit
+import CoreData
 
-class HistoryService {
+class HistoryServiceCoreData {
 
+    func myFetchRequest(search: String) -> [Words] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Words> = Words.fetchRequest()
+        fetchRequest.predicate = NSPredicate (format: "sourceWord CONTAINS[cd] %@ OR targetWord CONTAINS[cd] %@", search, search)
+        return try! context.fetch(fetchRequest)
+    }
+    
+    func getVocabulary() -> [Words] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Words> = Words.fetchRequest()
+        return try! context.fetch(fetchRequest)
+    }
+    
+    func cleanVocabulary() -> [Words] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Words")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do
+        {
+            let results = try context.fetch(fetchRequest)
+            for managedObject in results
+            {
+                let managedObjectData:NSManagedObject = managedObject as! NSManagedObject
+                context.delete(managedObjectData)
+            }
+        } catch let error as NSError {
+            //  print("Detele all my data in \(entity) error : \(error) \(error.userInfo)")
+        }
+        
+        let fetchRequestZ: NSFetchRequest<Words> = Words.fetchRequest()
+        return try! context.fetch(fetchRequestZ)
+    }
+    
 }
